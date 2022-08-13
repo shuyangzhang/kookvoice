403a91f5041667a3b7926efd1446701718c94d42
zhangshuyang <zhangshuyang@outlook.com>
2022-08-13 17:25:47 +0800

    feat: close ws connection before return

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package kookvoice
 func Play(token string, channelId string, input string) {
 	gatewayUrl := getGatewayUrl(token, channelId)
 	connect, rtpUrl := initWebsocketClient(gatewayUrl)
+	defer connect.Close()
 	go keepWebsocketClientAlive(connect)
 	go keepRecieveMessage(connect)
 	streamAudio(rtpUrl, input)
